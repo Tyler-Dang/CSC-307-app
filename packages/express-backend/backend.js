85841ebb450580f1398fd5c6a101befb10382ca4
Tyler-Dang <waztec1128@gmail.com>
2025-10-07 10:32:48 -0700

    Add a new Id field to users when created

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -2,36 +2,6 @@
 import express from "express";
 import cors from "cors";
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-
 const app = express();
 const port = 8000;
 
@@ -74,7 +44,7 @@ const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
+  const id = req.params["id"];
   let result = findUserById(id);
   if (result === undefined) {
     res.status(404).send("Resource not found.");
@@ -83,9 +53,14 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+const generateId = () => {
+  return Math.random()
+}
+
 const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
+  const userWithId = { ...user, id: generateId() };
+  users["users_list"].push(userWithId);
+  return userWithId;
 };
 
 const deleteUser = (id) => {
